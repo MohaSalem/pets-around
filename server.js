01cdf78b24791639a020465b2801f19ddef240e1
Mohamed Salem <mohammad.ramadan.mahmoud@gmail.com>
2021-11-09 00:01:13 +0200

    connect to the db

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => res.send('test run'));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is runing on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
