fb228692d19f13ff83197f2f7848a3fbcbbb39b3
Mohamed Salem <mohammad.ramadan.mahmoud@gmail.com>
2021-11-10 22:43:01 +0200

    connect to db, and implement backend functionality for user registeration

---
+++ routes/api/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+
+router.post('/register',
+    check('name', 'Name is required').notEmpty(),
+    check('email', 'Valid email is required').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6}),
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {name, email, password} = req.body;
+        try {
+            let user = await User.findOne({email});
+            if (user) {
+                return res.status(400).json({error: 'User exists'});
+            }
+
+            user = new User({
+                name,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+            res.send('User registration is successful');
+
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send('Server error');
+        }
+    }
+);
+
+module.exports = router;
